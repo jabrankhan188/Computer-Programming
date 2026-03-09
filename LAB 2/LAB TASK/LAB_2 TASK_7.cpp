413d6fb8994b26996f0a65c3b43e8510b3fcb245
jabrankhan188 <jabran12214@gmail.com>
2026-03-09 23:33:16 +0500

    Add files via upload

---
+++ LAB 2/LAB TASK/LAB_2 TASK_7.cpp
@@ -0,0 +1,19 @@
+// Name: Jabran Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0746
+// Section: A
+
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << "Talha Ali" << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
